c7b9f740361fa263899cc62a9fbd2d51b9878c78
Fatigue2k1 <116775697+Fatigue2k1@users.noreply.github.com>
2022-10-27 10:04:48 +0700

    Update softmaxReg.py

--- softmaxReg.py
+++ softmaxReg.py
@@ -2,16 +2,7 @@ import streamlit as st
 
 import datetime as dt
 
+original_list = ['A','B', 'C']
 
-display_text = 'whatever i am typing here'
-now = dt.datetime.now().strftime('$y-%m-$d %H:%M')
+result = st.selectbox('Search', original_list)
 
-st.write('Search')
-
-whatever = 'your name'
-print ('This is {whatever} python file.')
-
-original_list = ['Select Team','Parma', 'AC Milan']
-
-result = st.selectbox('Select your favourite team', original_list)
-st.write(f'La tua squadra favorita: {result}') 

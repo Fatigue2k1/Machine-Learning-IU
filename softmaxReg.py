0b85fd0a3d954a9d00d0bec64c8ef08d1af00318
Fatigue2k1 <116775697+Fatigue2k1@users.noreply.github.com>
2022-10-27 09:53:44 +0700

    Update softmaxReg.py

--- softmaxReg.py
+++ softmaxReg.py
@@ -5,3 +5,12 @@ import datetime as dt
 display_text = 'whatever i am typing here'
 now = dt.datetime.now().strftime('$y-%m-$d %H:%M')
 
+st.write(f'It is now {now} and I am typing: {display_text}')
+
+whatever = 'your name'
+print (f'This is {whatever} python file.')
+
+original_list = ['Select Team','Parma', 'AC Milan']
+
+result = st.selectbox('Select your favourite team', original_list)
+st.write(f'La tua squadra favorita: {result}') 
